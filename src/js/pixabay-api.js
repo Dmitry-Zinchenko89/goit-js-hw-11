66fb42c71692c99d30c628146e62bcc42b2826e3
Dmitry <zinchenko89d@gmail.com>
2025-04-20 13:16:41 +0300

    HW

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '49651039-b61b5892bf5245ff30c976637'; // Замінити на власний ключ
+const API_KEY = '49651039-b61b5892bf5245ff30c976637'; 
 
 export async function fetchImages(query) {
   const params = {
